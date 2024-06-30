c3e97db8fa16cda7943ba79108871c075d6c09ad
alexmaragia <maragialex@gmail.com>
2024-06-30 23:31:24 +0300

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Multiplication
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // 31 * 2 = 62
 
-// Write your code here
+// Generating a Random Integer
+const random = Math.floor(Math.random() * 100) + 1; // Generates a random integer greater than 0
+
+// Modulus Operation
+const num3 = 28;
+const num4 = 6;
+const mod = num3 % num4; // 28 % 6 = 4
+
+// Finding the Maximum Number
+const max = Math.max(5, 10, 15, 20); // Finds the highest number, which is 20
